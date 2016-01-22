eb0a4f3cd7b9aef4c344a63d502656d1bbb63cc0
Ace Nanter <ace.nanter@hotmail.fr>
2016-01-22 09:50:59 +0100

    Implémentation de la main Window, ajout d'un dossier Page avec un système de navigation TODO : Binding, ajout Police Star Wars, ajout champ stade, MVVM

---
+++ JediTournamentWPF/Pages/MatchCreator.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EntitiesLayer;
+using BusinessLayer;
+
+namespace JediTournamentWPF {
+    /// <summary>
+    /// Création de nouveaux matchs
+    /// </summary>
+    public partial class MatchCreator : Page {
+
+        private JediTournamentManager m_manager;
+        public MatchCreator(JediTournamentManager manager) {
+            InitializeComponent();
+            m_manager = manager;
+            comboJedi1.ItemsSource = m_manager.getNoSithsName();
+            comboJedi2.ItemsSource = m_manager.getSithsNames();
+
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e) {
+            this.NavigationService.GoBack();
+        }
+    }
+}
