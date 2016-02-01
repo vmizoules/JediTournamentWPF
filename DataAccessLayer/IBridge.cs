5e051b4d4a5e1eedba8de21e90913f28677ced30
Oumaima <elamrani.oumaimaa@gmail.com>
2016-02-01 19:57:21 +0100

    Debut d'implementation de La dataBase

--- DataAccessLayer/IBridge.cs
+++ DataAccessLayer/IBridge.cs
@@ -3,9 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EntitiesLayer;
 namespace DataAccessLayer {
     interface IBridge {
-        // TODO : méthodes interface
+        List<Jedi> GetAllJedis();
+        List<Stade> GetAllStades();
+        List<Match> GetAllMatchs();
+        List<Caracteristique> GetAllCaracteristiques();
+        List<Tournoi> GetAllTournois();
+        int UpdateListJedis(List<Jedi> _listeJedi);
+        int UpdateListStades(List<Stade> _listStade);
+        int UpdateListMatches(List<Match> _listMatch);
+        int UpdateListCaracteristiques(List<Caracteristique> _listCarac);
+        int UpdateListTournois(List<Tournoi> _listTournoi);
+        int RemoveListJedis(List<Jedi> _listeJedi);
+        int RemoveListStades(List<Stade> _listStade);
+        int RemoveListMatches(List<Match> _listMatch);
+        int RemoveListCaracteristiques(List<Caracteristique> _listCarac);
+        int RemoveListTournois(List<Tournoi> _listTournoi);
+        Utilisateur GetUtilisateurByLogin(string log);
     }
 }
--- DataAccessLayer/SQLAccess.cs
+++ DataAccessLayer/SQLAccess.cs
@@ -4,23 +4,100 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntitiesLayer;
 
 namespace DataAccessLayer {
     class SQLAccess : IBridge {
 
         private string m_connectionString = "";
 
-        /// <summary>
-        /// Constructeur
-        /// </summary>
-        SQLAccess(string connectionString) {
+         public SQLAccess(string connectionString) {
             m_connectionString = connectionString;
             using (SqlConnection sqlConnection = new SqlConnection(m_connectionString)) {
                 sqlConnection.Open();
-                
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(new SqlCommand("SELECT idJedi, Name, isSith, Pic FROM Jedis;", sqlConnection));
+
             }
         }
 
-        
+        public List<Caracteristique> GetAllCaracteristiques()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Jedi> GetAllJedis()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Match> GetAllMatchs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Stade> GetAllStades()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tournoi> GetAllTournois()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Utilisateur GetUtilisateurByLogin(string log)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveListCaracteristiques(List<Caracteristique> _listCarac)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveListJedis(List<Jedi> _listeJedi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveListMatches(List<Match> _listMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveListStades(List<Stade> _listStade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveListTournois(List<Tournoi> _listTournoi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateListCaracteristiques(List<Caracteristique> _listCarac)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateListJedis(List<Jedi> _listeJedi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateListMatches(List<Match> _listMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateListStades(List<Stade> _listStade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateListTournois(List<Tournoi> _listTournoi)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
