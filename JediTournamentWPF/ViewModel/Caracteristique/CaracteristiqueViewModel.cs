13950572aeef3bbf435741d52902fbe9f35ba6ed
Vincent <dark-vince15@hotmail.fr>
2016-02-03 23:25:25 +0100

    Move view model for caracteristiques

---
+++ JediTournamentWPF/ViewModel/Caracteristique/CaracteristiqueViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JediTournamentWPF.ViewModel {
+    class CaracteristiqueViewModel : ViewModelBase
+    {
+        private EntitiesLayer.Caracteristique _carac;
+
+        public EntitiesLayer.Caracteristique Carac
+        {
+            get { return _carac; }
+            private set
+            {
+                _carac = value;
+            }
+        }
+        public string Nom
+        {
+
+            get { return Carac.Nom; }
+            set
+            {
+                if (value == Carac.Nom) return;
+                Carac.Nom = value;
+                OnPropertyChanged("Nom");
+            }
+        }
+
+        public int Valeur
+        {
+            get { return Carac.Valeur; }
+            set
+            {
+                if (value == Carac.Valeur) return;
+                Carac.Valeur = value;
+                OnPropertyChanged("Nom");
+                OnPropertyChanged("Valeur");
+            }
+        }
+
+        public CaracteristiqueViewModel(EntitiesLayer.Caracteristique caract) {
+            Carac = caract;
+        }
+    }
+}
